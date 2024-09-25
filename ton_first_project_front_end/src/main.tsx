38a43cd767806181a1374162d5538f76375167df
Nomad55555 <beidu55@hotmail.com>
2024-09-25 16:25:23 -0300

    Workflow

--- ton_first_project_front_end/src/main.tsx
+++ ton_first_project_front_end/src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = "https://Nomad55555.github.io/ton_first_project_front_end/tonconnect-manifest.json";
+const manifestUrl = "https://Nomad55555.github.io/ton_project_front_end/ton_first_project_front_end/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
